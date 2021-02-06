e363a96a43e47e7e0d07dc14816f47049e2bfb4d
SamedHrmn <samed_harman@hotmail.com>
2021-02-06 16:57:00 +0300

    Added profile screen components

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fidanla_app/core/constants/assets_constants.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -11,10 +12,89 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Profil"),
-      ),
-      body: Text("Profile"),
-    );
+        appBar: AppBar(
+          title: Text("Profil"),
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.3,
+              color: Colors.grey[300],
+              child: Row(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                          margin: EdgeInsets.only(left: 12),
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image:
+                                    AssetImage(AssetConstanst.LOGIN_ICON_PATH)),
+                          )),
+                      Text("TechnoVation"),
+                      Text("Toplam dikim sayısı: 25"),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 50),
+                    child: Row(
+                      children: [
+                        Icon(Icons.star),
+                        Icon(Icons.star),
+                        Icon(Icons.star)
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+                child: Container(
+              color: Colors.blue,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: 45,
+                      child: Card(
+                        elevation: 8,
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Container(
+                                width: 25,
+                                height: 25,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  color: Colors.pink,
+                                ),
+                                child: Center(child: Text(index.toString()))),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text("Sana fidan aldım"),
+                            Spacer(),
+                            Text("20/04/2021"),
+                            SizedBox(
+                              width: 10,
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            )),
+          ],
+        ));
   }
 }
